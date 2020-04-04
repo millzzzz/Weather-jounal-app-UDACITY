c96dd9bc02ec510b66d1a288d9b52f969cb91a85
Senik <senik@Seniks-MBP.lan>
2020-04-04 16:33:57 -0500

    set up server

--- server.js
+++ server.js
@@ -2,18 +2,35 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8080;
+const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) });
+
+//POST route that adds incoming data into the object projectData
+app.post('/add', (req, res)=> {
+    projectData['date'] = req.body.date;
+    projectData['temp'] = req.body.temp;
+    projectData['userResponse'] = req.body.userResponse;
+    res.send(projectData);
+});
+
+//GET route that returns an object of projectData
+app.get('/all', (req, res) => {
+    res.send(projectData);
+});
